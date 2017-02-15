4e927cc09940e526d0b7cb44c4bcdcd8a347718c
DocJaded <lokisson@gmail.com>
2017-02-15 14:50:11 +0100

    asd
    asd

---
+++ 2dArray/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2dArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[,] arrayName = new string[5, 2]
+            {
+                { "Tits", "Boobs" },
+                { "Legs", "Gams" }, 
+                { "Assholes", "Sphincters" },
+                { "Nipples", "Milk Glands" },
+                { "Ass", "Butt" }
+            };
+
+
+
+            
+
+            for (int i = 0; i < 6; i++)
+            {
+                
+                for (int j = 0; j < 2; j++)
+                {
+
+                    Console.WriteLine(arrayName[i, j]);
+                    
+                    if (arrayName[i, j] == "Ass")
+                    {
+
+                        Console.WriteLine("Ass found at " +"["+i+"," +j+"]"); 
+                        return;
+
+                    }
+                    else Console.WriteLine("Ass not found");
+                }
+            }
+
+
+        }
+    }
+}
+    
